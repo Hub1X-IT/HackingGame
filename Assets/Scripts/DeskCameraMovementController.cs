5790828933853d9fb2b9b8a882245490b2ad26f7
Widzew12 <szmnglowacz@gmail.com>
2024-10-15 17:33:06 +0200

    Further desk camera controller development
    15.10.2024 17:32

--- Assets/Scripts/DeskCameraMovementController.cs
+++ Assets/Scripts/DeskCameraMovementController.cs
@@ -2,49 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
-public class PlayerController : MonoBehaviour {
+public class DeskCameraMovementController : MonoBehaviour {
 
 
     [SerializeField] private Transform cameraFollowObject;
 
-
-    private CharacterController characterController;
-
-    
-    private Vector3 moveDirection;
-    [SerializeField] private float moveSpeed = 5f;
-
-
     private Vector2 rotationInput;
 
 
-    private const float gravity = -9.81f;
-    [SerializeField] private float gravityMultiplier = 1f;
-    private float verticalVelocity;
-
-
-    private void Awake() {
-        characterController = GetComponent<CharacterController>();
-    }
-
-
     private void Update() {
-        HandleGravity();
         HandleRotation();
-        HandleMovement();
     }
 
-
-    private void HandleMovement() {        
-        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
-        Vector3 movementInput = new Vector3(inputVector.x, 0f, inputVector.y);
-        
-        moveDirection = transform.right * movementInput.x + transform.forward * movementInput.z;
-        moveDirection.y = verticalVelocity;
-        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
-    }
-    
 
     private void HandleRotation() {
         rotationInput = GameInput.Instance.GetRotationVector();
@@ -55,11 +24,11 @@ public class PlayerController : MonoBehaviour {
 
         // Handle X axis rotation - rotating only the camera
         Vector3 cameraRotation = cameraFollowObject.rotation.eulerAngles;
-        
+
         if (cameraRotation.x > 180f) {
             cameraRotation.x -= 360f;
         }
-        {   
+        {
             // eulerAngles always maps rotation to a positive value so if it is more than 180 degrees, you have to make it negative so that you can clamp it
             // For example: when rotation equals -20, you get 340 which is greater than 90 so it would be clamped to 90 degrees, although it should have been left as it is
         }
@@ -71,17 +40,5 @@ public class PlayerController : MonoBehaviour {
         cameraRotation.x = Mathf.Clamp(cameraRotation.x, minXRotation, maxXRotation);
 
         cameraFollowObject.rotation = Quaternion.Euler(cameraRotation);
-    }
-
-
-    private void HandleGravity() {
-        if (characterController.isGrounded && verticalVelocity < 0f) {
-            verticalVelocity = -0.1f;
-        }
-        else {
-            verticalVelocity += gravity * gravityMultiplier * Time.deltaTime;
-        }
-        moveDirection.y = verticalVelocity;
-        // Debug.Log(verticalVelocity);
     }
 }
